9c9bed67ee8e3b53ef04d00c057697b656345111
NguyenPhuc <phucnguyentrong1804@gmail.com>
2024-04-20 15:15:00 +0700

    fix icon

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -4,8 +4,4 @@ import vitePluginRequire from "vite-plugin-require";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginRequire.default()],
-  build: {
-    // Đường dẫn cơ sở của ứng dụng khi triển khai
-    outDir: "build",
-  },
 });
