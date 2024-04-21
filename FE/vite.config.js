dd871af2e5de3d1e4fac3e89f8a617bac728aa4f
NguyenPhuc <phucnguyentrong1804@gmail.com>
2024-04-21 23:57:35 +0700

    fix env

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginRequire from "vite-plugin-require";
-import EnvironmentPlugin from "vite-plugin-environment";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePluginRequire.default(),
-    EnvironmentPlugin("all", { prefix: "VITE_" }),
-  ],
+  plugins: [react(), vitePluginRequire.default()],
 });
