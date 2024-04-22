1ce6da82b06c9158b8e1e22a9d6f500ee1df2cdf
NguyenPhuc <phucnguyentrong1804@gmail.com>
2024-04-22 13:27:31 +0700

    fix

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "@material-tailwind/react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./tranlation/i18n.js";
 import { HelmetProvider } from "react-helmet-async";
+import { ThemeProvider } from "@material-tailwind/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <ThemeProvider>
